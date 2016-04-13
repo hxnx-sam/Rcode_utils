c52e75ee1034726c16af4a0000291a6a1a01ca9c
Samantha Lycett <sam_lycett@hotmail.com>
2016-04-13 18:45:26 +0100

    Update to bird species & continents - including header

--- birdSpecies_and_continents.R
+++ birdSpecies_and_continents.R
@@ -1,3 +1,12 @@
+# Rcode_utils is free software: you can redistribute it and/or modify it under the terms of the 
+# GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or any later version.
+# Rcode_utils is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+# See the GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License along with Rcode_utils.  
+# If not, see <http://www.gnu.org/licenses/>.
+
+
 # functions to classify bird names to latin species names and countries to continents
 # S. J. Lycett
 # 14 Dec 2011 - extract from from matrixTranslate.R
